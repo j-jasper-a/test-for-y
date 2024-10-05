31bd204d78662252573670e47fe62988da825fe8
Jihan Jasper Al Rashid <j.jasper.a@gmail.com>
2024-10-05 12:57:43 +0600

    feat: implement internationalization

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,36 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter as FontMain } from "next/font/google";
+
+const fontMain = FontMain({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-main",
+});
+
+export const metadata: Metadata = {
+  title: "Avatown | The Marketplace",
+  description: "Avatown is a marketplace for VR avatars.",
+};
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body className={`${fontMain.className} antialiased`}>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
